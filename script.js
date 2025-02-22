13904359182efca6d6f2e7f1f417f4a3dce9865b
LandonG121 <landongarcia0011@gmail.com>
2025-02-22 21:26:21 +0000

    Finshed project

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-
-
-
-
-
-
-
-
+let playlist = []
+function addSong() {
+    let title = document.getElementById("songTitle").value;
+    let artist = document.getElementById("artist").value;
+    let duration = document.getElementById("duration").value;
+    let Song = {
+        songTitle: title,
+        songArtist: artist,
+        songDuration: duration,
+    }
+    if (title == '') {
+        alert("Fill in the title box first.")
+        return
+    }
+    if (artist == '') {
+        alert("Fill in the artist box first.")
+        return
+    }
+    if (duration == '') {
+        alert("Fill in the duration box first.")
+        return
+    }
+    playlist.push(Song);
+    displayPlaylist()
+    document.getElementById('songTitle').value = '';
+    document.getElementById('artist').value = '';
+    document.getElementById('duration').value = '';
+}
 
 
 
